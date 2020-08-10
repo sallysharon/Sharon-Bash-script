25253f377ba4c6be76a1d7c45fe57f53578e1f3d
Festus Were <fwere@cellulant.com>
2020-08-10 13:36:00 +0300

    chore: added new pipeline

--- cicd.sh
+++ cicd.sh
@@ -1,33 +1,41 @@
 #!/bin/bash
 set -e
+GITLABFILE=./.gitlab-ci.yml
+if test -f "$GITLABFILE"; then
+    echo "$GITLABFILE exists."
+    exit
+fi
 
-echo "Please Enter the project name: e.g singelsmsapi NOTE: DO NOT use underscores or spaces."
+DEPLOYDIR=./deploy/development.config
+if test -f "$DEPLOYDIR"; then
+    echo "Deploy directory exists."
+    exit
+fi
+
+echo "Please Enter the project name: | e.g singelsmsapi NOTE: DO NOT use underscores or spaces. | "
 read projectname
+export PROJECTNAME=$projectname
 
-echo "Please Enter the project group: e.g messaging this is the software group the project belongs to. NOTE: DO NOT use underscores or spaces."
+echo "Please Enter the project group: | e.g messaging this is the software group the project belongs to. NOTE: DO NOT use underscores or spaces.| "
 read projectgroup
+export PROJECTGROUP=$projectgroup
 
-PS3="Select Pipeline type e.g | docker | jib-maven | jib-gradle |"
-options=("docker" "jib-maven" "jib-gradle" "Quit")
+PS3="Enter number for Pipeline type: | e.g |1. docker |2. jib-maven |3. jib-gradle | "
+options=("docker" "jib-maven" "jib-gradle")
 select opt in "${options[@]}"
 do
     case $opt in
         "docker")
             echo "$REPLY: $opt pipeline seclected."
             export PIPELINETYPESELECT=$opt
-           
             ;;
         "jib-maven")
             echo "$REPLY: $opt pipeline seclected."
             export PIPELINETYPESELECT=$opt
-           
             ;;
         "jib-gradle")
             echo "$REPLY: $opt pipeline seclected."
             export PIPELINETYPESELECT=$opt
-            ;;
-        "Quit")
-            break
             ;;
         *) echo "invalid option $REPLY";;
     esac
@@ -46,17 +54,6 @@ COMMAND="git clone --depth 1 -b $BRANCH https://$DOWNLOAD_URL$EXT ."
 
 echo "Setting Up CI/CD base project from $DOWNLOAD_URL"
 
-GITLABFILE=./.gitlab-ci.yml
-if test -f "$GITLABFILE"; then
-    echo "$GITLABFILE exists."
-    exit
-fi
-
-DEPLOYDIR=./deploy/development.config
-if test -f "$DEPLOYDIR"; then
-    echo "Deploy directory exists."
-    exit
-fi
 
 mkdir ./deploy
 cd ./deploy
@@ -76,21 +73,21 @@ cd ../
 
 
 #configure gitlab yml
-sed -i 's@PROJECT@'"$projectname"'@g' ./.gitlab-ci.yml
-sed -i 's@GROUP@'"$projectgroup"'@g' ./.gitlab-ci.yml
+sed -i 's@PROJECT@'"$PROJECTNAME"'@g' ./.gitlab-ci.yml
+sed -i 's@GROUP@'"$PROJECTGROUP"'@g' ./.gitlab-ci.yml
 sed -i 's@PIPELINETYPE@'"$PIPELINETYPESELECT"'@g' ./.gitlab-ci.yml
 
 #configure config variables
-sed -i 's@PROJECT@'"$projectname"'@g' ./deploy/*.config
-sed -i 's@GROUP@'"$projectgroup"'@g' ./deploy/*.config
+sed -i 's@PROJECT@'"$PROJECTNAME"'@g' ./deploy/*.config
+sed -i 's@GROUP@'"$PROJECTGROUP"'@g' ./deploy/*.config
 
 #configure helm chart variables
 for i in ./deploy/charts/demo/*; do
-    sed -i 's@demo@'"$projectname"'@g' $i
+    sed -i 's@demo@'"$PROJECTNAME"'@g' $i
 done
 
 cd ./deploy/charts
-mv demo $projectname
+mv demo $PROJECTNAME
 cd ../../
 
 echo ""
