38d482a15dcd5bf1af1ab6513f77abb036d3d78b
Festus Were <fwere@cellulant.com>
2020-08-10 15:17:01 +0300

    chore: added new CICD variable prefix to cicd.sh

--- cicd.sh
+++ cicd.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
-GITLABFILE=./.gitlab-ci.yml
-if test -f "$GITLABFILE"; then
+CICD_GITLABFILE=./.gitlab-ci.yml
+if test -f "$CICD_GITLABFILE"; then
     echo "Pre-check run ..."
-    echo "$GITLABFILE exists."
+    echo "$CICD_GITLABFILE exists."
     exit
 fi
 
-DEPLOYDIR=./deploy/development.config
-if test -f "$DEPLOYDIR"; then
+CICD_DEPLOYDIR=./deploy/development.config
+if test -f "$CICD_DEPLOYDIR"; then
     echo "Pre-check run ..."
     echo "Deploy directory exists."
     exit
@@ -16,11 +16,11 @@ fi
 
 echo "Please Enter the project name: | e.g singelsmsapi NOTE: DO NOT use underscores or spaces. | "
 read projectname
-export PROJECTNAME=$projectname
+export CICD_PROJECTNAME=$projectname
 
 echo "Please Enter the project group: | e.g messaging this is the software group the project belongs to. NOTE: DO NOT use underscores or spaces.| "
 read projectgroup
-export PROJECTGROUP=$projectgroup
+export CICD_PROJECTGROUP=$projectgroup
 
 PS3="Enter number for Pipeline type: | e.g |1. docker |2. jib-maven |3. jib-gradle | "
 options=("docker" "jib-maven" "jib-gradle")
@@ -29,17 +29,17 @@ do
     case $opt in
         "docker")
             echo "$REPLY: $opt pipeline seclected."
-            export PIPELINETYPESELECT=$opt
+            export CICD_PIPELINETYPESELECT=$opt
             break
             ;;
         "jib-maven")
             echo "$REPLY: $opt pipeline seclected."
-            export PIPELINETYPESELECT=$opt
+            export CICD_PIPELINETYPESELECT=$opt
             break
             ;;
         "jib-gradle")
             echo "$REPLY: $opt pipeline seclected."
-            export PIPELINETYPESELECT=$opt
+            export CICD_PIPELINETYPESELECT=$opt
             break
             ;;
         *) echo "invalid option $REPLY";;
@@ -49,20 +49,20 @@ done
 
 # Download latest ci/cd project from Gitlab
 
-EXT=".git"
+CICD_EXT=".git"
 
-BRANCH="master"
+CICD_BRANCH="master"
 
-DOWNLOAD_URL="git.prod.cellulant.com/ops-templates/ci-cd-workflows/rok8s-scripts/getting-started"
+CICD_DOWNLOAD_URL="git.prod.cellulant.com/ops-templates/ci-cd-workflows/rok8s-scripts/getting-started"
 
-COMMAND="git clone --depth 1 -b $BRANCH https://$DOWNLOAD_URL$EXT ."
+CICD_COMMAND="git clone --depth 1 -b $CICD_BRANCH https://$CICD_DOWNLOAD_URL$EXT ."
 
-echo "Setting Up CI/CD base project from $DOWNLOAD_URL"
+echo "Setting Up CI/CD base project from $CICD_DOWNLOAD_URL"
 
 
 mkdir ./deploy
 cd ./deploy
-$COMMAND
+$CICD_OMMAND
 cd ./deploy
 
 echo ""
@@ -72,53 +72,56 @@ cd ../
 mv .gitlab-ci.yml ../
 
 echo "phase1: cleaning up"
-rm -rf $EXT
+rm -rf $CICD_EXT
 rm -rf ./deploy
-rm -f ./template.gitlab-ci.yml
 cd ../
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	OS="linux"
-    export SED='sed -i' 
-    echo "OS | '$OS'. sed | '$SED'. "
+    export CICD_SED='sed -i' 
+    echo "OS | '$OS'. sed | '$CICD_SED'. "
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	OS="darwin"
-    export SED="sed -i '' -e"
-    echo "OS | '$OS'. sed | '$SED'. "
+    export CICD_SED="sed -i '' -e"
+    echo "OS | '$OS'. sed | '$CICD_SED'. "
 elif [[ "$OSTYPE" == "win32" ]] || [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]] ; then
 	OS="windows"
-	export SED='sed -i ' 
-    echo "OS | '$OS'. sed | '$SED'. "
+	export CICD_SED='sed -i ' 
+    echo "OS | '$OS'. sed | '$CICD_SED'. "
 else
 	echo "No sed command available for OS '$OSTYPE'. using default."
-    export SED='sed -i '
-    echo "OS | '$OS'. sed | '$SED'. " 
+    export CICD_SED='sed -i '
+    echo "OS | '$OS'. sed | '$CICD_SED'. " 
   exit
 fi
 
 
 echo "executing CI/CD config"
 #configure gitlab yml
-$SED 's@PROJECT@'"$PROJECTNAME"'@g' ./.gitlab-ci.yml
-$SED 's@GROUP@'"$PROJECTGROUP"'@g' ./.gitlab-ci.yml
-$SED 's@PIPELINETYPE@'"$PIPELINETYPESELECT"'@g' ./.gitlab-ci.yml
+$CICD_SED 's@PROJECT@'"$CICD_PROJECTNAME"'@g' ./.gitlab-ci.yml
+$CICD_SED 's@GROUP@'"$CICD_PROJECTGROUP"'@g' ./.gitlab-ci.yml
+$CICD_SED 's@PIPELINETYPE@'"$CICD_PIPELINETYPESELECT"'@g' ./.gitlab-ci.yml
 
 #configure config variables
-$SED 's@PROJECT@'"$PROJECTNAME"'@g' ./deploy/*.config
-$SED 's@GROUP@'"$PROJECTGROUP"'@g' ./deploy/*.config
+$CICD_SED 's@PROJECT@'"$CICD_PROJECTNAME"'@g' ./deploy/*.config
+$CICD_SED 's@GROUP@'"$CICD_PROJECTGROUP"'@g' ./deploy/*.config
 
 #configure helm chart variables
 for i in ./deploy/charts/demo/*.yaml; do
-    $SED 's@demo@'"$PROJECTNAME"'@g' $i
+    $CICD_SED 's@demo@'"$CICD_PROJECTNAME"'@g' $i
 done
 
 for i in ./deploy/charts/demo/templates/*.yaml; do
-    $SED 's@demo@'"$PROJECTNAME"'@g' $i
+    $CICD_SED 's@demo@'"$CICD_PROJECTNAME"'@g' $i
 done
+$CICD_SED 's@demo@'"$CICD_PROJECTNAME"'@g' ./deploy/charts/demo/templates/tests/*.yaml
+$CICD_SED 's@demo@'"$CICD_PROJECTNAME"'@g' ./deploy/charts/demo/templates/_helpers.tpl
 
 echo "phase2: cleaning up"
+rm -f ./deploy/template.gitlab-ci.yml
+rm -f ./deploy/cicd.sh
 cd ./deploy/charts
-mv demo $PROJECTNAME
+mv demo $CICD_PROJECTNAME
 cd ../../
 
 echo ""
